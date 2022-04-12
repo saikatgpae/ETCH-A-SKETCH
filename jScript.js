a592951a0f0ba6510974eba4fa4ea62102060ea6
Saikat Chakraborty <saikatgpae@gmail.com>
2022-04-12 21:48:41 +0530

    added a div and its class to index.html using javascript

--- jScript.js
+++ jScript.js
@@ -1,1 +1,10 @@
 console.log('Hello World!')
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    
+    const div = document.createElement('div');
+    document.body.appendChild(div);
+    const att = document.createAttribute("class");
+    att.value = "container";
+    document.getElementsByTagName("div")[0].setAttributeNode(att);
+})
