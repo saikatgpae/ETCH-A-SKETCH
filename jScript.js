cefb5ad75693ae5edbe4303527ab4ce96f6e08d6
Saikat Chakraborty <saikatgpae@gmail.com>
2022-04-13 23:09:27 +0530

    created the grid of 16*16 inside the div of class grid-container

--- jScript.js
+++ jScript.js
@@ -2,9 +2,23 @@ console.log('Hello World!')
 
 document.addEventListener('DOMContentLoaded', ()=>{
     
-    const div = document.createElement('div');
-    document.body.appendChild(div);
+    // creating the main div with class name main
+    const main = document.createAttribute('class');
+    main.value = "main";
+    const main_div = document.createElement('div');
+    document.body.appendChild(main_div).setAttributeNode(main);
+    
+    //creating the grid container with class name grid-container
     const att = document.createAttribute("class");
-    att.value = "container";
-    document.getElementsByTagName("div")[0].setAttributeNode(att);
+    att.value = "grid-container";
+    const div = document.createElement('div');
+    document.querySelector('.main').appendChild(div).setAttributeNode(att);
+    
+    // creating 16*16 256 div inside the div of class grid-container
+    for(let i = 1; i<=256; i++){
+        const grid_div_attr = document.createAttribute('class');
+        grid_div_attr.value = 'grid-div';
+        const grid_div = document.createElement('div');
+        document.querySelector('.grid-container').appendChild(grid_div).setAttributeNode(grid_div_attr)
+    }
 })
