914266d02c6bb25e412c27be2a3612a3a0823b73
Saikat Chakraborty <saikatgpae@gmail.com>
2022-04-17 22:02:56 +0530

    added the funtion to create the new grid using the user input

--- jScript.js
+++ jScript.js
@@ -1,15 +1,37 @@
 //console.log('Hello World!')
 function grid(){
-    grid_number = parseInt(prompt('Please enter the grid matrix between 1-100'))
+    //taking the input to create the grid and storing the input
+    var grid_number = parseInt(prompt('Please enter the grid matrix between 1-100'));
+
+    //removing all child node inside the main div
+    const element = document.querySelector(".main");
+    while(element.firstChild){element.removeChild(element.firstChild)};
+
+    //creating the new grid using the input
+    const frag = document.createDocumentFragment();
+    for(let i = 1; i<=grid_number*grid_number; i++){
+        const grid_div_attr = document.createAttribute('class');
+        grid_div_attr.value = 'new-grid';
+        const grid_div = document.createElement('div');
+        frag.appendChild(grid_div).setAttributeNode(grid_div_attr);   
+    }
+    document.querySelector('.main').appendChild(frag);
+
+    //fixing the height and width of the grid using the user input
+    var new_grid = document.querySelectorAll('.new-grid');
+    var x=960/grid_number;
+    Array.from(new_grid).forEach(v => v.style.width = x+'px');
+    Array.from(new_grid).forEach(v => v.style.height = x+'px');    
 }
 
 document.addEventListener('DOMContentLoaded', ()=>{
     
-    // creating the main div with class name main
+    /*// creating the main div with class name main
     const main = document.createAttribute('class');
     main.value = "main";
     const main_div = document.createElement('div');
     document.body.appendChild(main_div).setAttributeNode(main);
+    */
     
     //creating the grid container with class name grid-container
     const att = document.createAttribute("class");
@@ -19,8 +41,6 @@ document.addEventListener('DOMContentLoaded', ()=>{
     
     // creating 16*16 256 div inside the div of class grid-container
     const frag = document.createDocumentFragment();
-
-    //const frag = createDocumentFragment();
     for(let i = 1; i<=256; i++){
         const grid_div_attr = document.createAttribute('class');
         grid_div_attr.value = 'grid-div';
@@ -36,4 +56,5 @@ document.addEventListener('DOMContentLoaded', ()=>{
     }))
     //var arr = Array.from(grid);
     //console.log(arr.length);
+    
 })
